cc47a8f81db7aabb673368e8321eee18f187134b
Julian Straub <jstraub@csail.mit.edu>
2015-04-05 15:35:09 -0400

    README updated

---
+++ README.md
@@ -0,0 +1,71 @@
+This code extracts the local Manhattan World rotation and segmentation from
+depth images in real-time.
+
+If you are using this code for your research please cite
+```
+
+```
+
+### Dependencies
+
+This code depends on the following other libraries and was tested under Ubuntu
+14.04. 
+- pcl 1.7 (and vtk 5.8)
+- Opencv 2 (2.3.1)
+- Eigen3 (3.0.5) 
+- cuda 5.5 or 6.5 
+- Boost (1.52)
+
+The GPU kernels were tested on a Nvidia Quadro K2000M with compute
+capability 3.0.
+
+### Install
+
+Once you have those dependencies in place run
+```
+make checkout && make configure && make 
+```
+This will checkout dependencies from some of my other repos and compile
+everything to ./build/
+
+### Library
+*libcudaPcl.so* collects all the cuda code into one shared library. The rest
+of the code is in the form of header files.
+
+### Executables
+- *realtimeMF*: infers the Manhattan Frame rotation and implied scene
+  segmentation in real-time from depth images. If no input is specified via
+  '-i', the program uses openni to grab frames from a connected RGB-D camera.
+  The different modes refer to the different underlying models as described in
+  the paper (see reference above).
+```
+Allowed options:
+  -h [ --help ]         produce help message
+  -m [ --mode ] arg     mode of the rtmf (direct, approx, vmf)
+  -i [ --in ] arg       path to input file
+  -o [ --out ] arg      path to output file
+  -d [ --display ]      display results
+  -B [ --B ] arg        B for guided filter
+  -T [ --T ] arg        number of iterations
+  --eps arg             eps for guided filter
+  -f [ --f_d ] arg      focal length of depth camera
+  --nCGIter arg         max number of CG iterations
+  --dt arg              steplength for linesearch
+  --tMax arg            max length for linesearch
+```
+- *realtimeMF_lcm*: same as realtimeMF except that the RGB-D frames are
+  obtained as LCM (https://github.com/lcm-proj/lcm) messages.
+```
+Allowed options:
+  -h [ --help ]         produce help message
+  -m [ --mode ] arg     mode of the rtmf (direct, approx, vmf)
+  -o [ --out ] arg      path to output file
+  -d [ --display ]      display results
+  -B [ --B ] arg        B for guided filter
+  -T [ --T ] arg        number of iterations
+  --eps arg             eps for guided filter
+  -f [ --f_d ] arg      focal length of depth camera
+  --nCGIter arg         max number of CG iterations
+  --dt arg              steplength for linesearch
+  --tMax arg            max length for linesearch
+```
--- src/realtimeMF_lcm.cpp
+++ src/realtimeMF_lcm.cpp
@@ -16,11 +16,15 @@
 
 #include <rtmf/rtmf.hpp>
 
+#include <zlib.h>
+#include <glib.h>
+#include <rtmf/jpeg-utils-ijg.h>
+
 class RealtimeMF_lcm
 {
   public:
     RealtimeMF_lcm(shared_ptr<lcm::LCM>& lcm, 
-        shared_ptr<RealtimeMF>& pRtmf);
+        shared_ptr<RealtimeMF>& pRtmf, string outPath="");
     virtual ~RealtimeMF_lcm();
 
     virtual void rgbd_cb(const lcm::ReceiveBuffer* rbuf, const
@@ -31,6 +35,7 @@ class RealtimeMF_lcm
 
    shared_ptr<lcm::LCM> lcm_;
    shared_ptr<RealtimeMF> pRtmf_;
+   string outPath_;
 
    cv::Mat rgb_;
    cv::Mat d_;
@@ -39,8 +44,8 @@ class RealtimeMF_lcm
 
 
 RealtimeMF_lcm::RealtimeMF_lcm(shared_ptr<lcm::LCM>& lcm,
-    shared_ptr<RealtimeMF>& pRtmf)
-  : lcm_(lcm), pRtmf_(pRtmf)
+    shared_ptr<RealtimeMF>& pRtmf, string outPath)
+  : lcm_(lcm), pRtmf_(pRtmf), outPath_(outPath)
 {
   lcm_->subscribe( "KINECT_FRAME",&RealtimeMF_lcm::rgbd_cb,this);
 };
@@ -61,39 +66,59 @@ void RealtimeMF_lcm::rgbd_cb(const lcm::ReceiveBuffer* rbuf, const
   if (msg->image.image_data_format == kinect::image_msg_t::VIDEO_RGB)
   {
     cout << "raw rgb image "<<endl;
-    rgb_ = cv::Mat(msg->image.height, msg->image.width, CV_8UC3,
-        const_cast<uint8_t*>(&(msg->image.image_data[0])));
+    rgb_ = cv::Mat(msg->image.height, msg->image.width, CV_8UC3);
+//        const_cast<uint8_t*>(&(msg->image.image_data[0])));
+    memcpy(rgb_.data, &(msg->image.image_data[0]), msg->image.width *
+        msg->image.height * 3);
   }else if (msg->image.image_data_format == kinect::image_msg_t::VIDEO_RGB_JPEG){
     cout << "jpg rgb image "<<endl;
+    rgb_ = cv::Mat(msg->image.height, msg->image.width, CV_8UC3);
+    jpegijg_decompress_8u_rgb (&(msg->image.image_data[0]),
+          msg->image.image_data_nbytes, rgb_.data, msg->image.width,
+          msg->image.height, msg->image.width * 3);
   }else{
     cout << "Format not recognized: " << msg->image.image_data_format << endl;
+    rgb_ = cv::Mat::zeros(msg->image.height, msg->image.width, CV_8UC3);
   }
 
-  if (msg->depth.depth_data_format == kinect::depth_msg_t::DEPTH_MM)
+  if(msg->depth.compression == KINECT_DEPTH_MSG_T_COMPRESSION_NONE)
+//  if (msg->depth.depth_data_format == kinect::depth_msg_t::DEPTH_MM)
   {
-    cout << "MM depth image"<<endl;
+    cout << "MM depth image"<<msg->depth.height<<"x"<< msg->depth.width<<endl;
     d_ = cv::Mat(msg->depth.height, msg->depth.width, CV_16SC1,
         const_cast<uint8_t*>(&(msg->depth.depth_data[0])));
     cv::imshow("depth",d_);
   }else{
-    cout << "Format not recognized: " << msg->depth.depth_data_format << endl;
+    cout << " depth compressed: " << msg->depth.depth_data_format
+      <<msg->depth.height<<"x"<< msg->depth.width<<endl;
+    unsigned long dlen = msg->depth.uncompressed_size;
+    d_ = cv::Mat(msg->depth.height, msg->depth.width, CV_16SC1);
+    uncompress(d_.data, &dlen, &(msg->depth.depth_data[0]),
+        msg->depth.depth_data_nbytes);
   }
 
   pRtmf_->compute(d_);
 //  this->update();
 //  cout<<pRtmf_->cRmf()<<endl;
 //
-  cout<<"visualize Normals"<<endl;
-  cv::Mat Iseg = pRtmf_->overlaySeg(this->rgb_);
-  cv::imshow("seg",Iseg);
-  cv::waitKey(10);
+ visualizeNormals(); cv::waitKey(10);
 }
 
 void RealtimeMF_lcm::visualizeNormals()
 {
+  static int frameNr = 0;
   cout<<"visualize Normals"<<endl;
   cv::Mat Iseg = pRtmf_->overlaySeg(this->rgb_);
   cv::imshow("seg",Iseg);
+  if (outPath_.compare("") != 0)
+  {
+    stringstream ss;
+    ss << outPath_ << "./frame"
+      << std::setw(7) << std::setfill('0') << frameNr<<".png";
+    cout<<"writing to "<<ss.str()<<endl;
+    cv::imwrite(ss.str().data(),Iseg);
+  }
+  ++ frameNr;
 }
 
 #include <boost/program_options.hpp>
@@ -108,7 +133,7 @@ int main(int argc, char ** argv) {
     ("help,h", "produce help message")
     ("mode,m", po::value<string>(), 
     "mode of the rtmf (direct, approx, vmf)")
-    ("in,i", po::value<string>(), "path to input file")
+//    ("in,i", po::value<string>(), "path to input file")
     ("out,o", po::value<string>(), "path to output file")
     ("display,d", "display results")
     ("B,B", po::value<int>(), "B for guided filter")
@@ -129,8 +154,8 @@ int main(int argc, char ** argv) {
     return 1;
   }
 
-  string mode = "approx";
-  string path = "";
+  string mode = "vmf";
+  string outPath = "";
   cudaPcl::CfgSmoothNormals cfgNormals;
   cfgNormals.f_d = 540.;
   cfgNormals.eps = 0.2*0.2;
@@ -140,7 +165,7 @@ int main(int argc, char ** argv) {
   CfgOptSO3 cfgOptSO3;
   cfgOptSO3.sigma = 5.0f*M_PI/180.0;
   if(vm.count("mode")) mode = vm["mode"].as<string>();
-  if(vm.count("in")) path = vm["in"].as<string>();
+  if(vm.count("out")) outPath = vm["out"].as<string>();
   if(vm.count("eps")) cfgNormals.eps = vm["eps"].as<float>();
   if(vm.count("f_d")) cfgNormals.f_d = vm["f_d"].as<float>();
   if(vm.count("B")) cfgNormals.B = uint32_t( vm["B"].as<int>());
@@ -172,7 +197,7 @@ int main(int argc, char ** argv) {
     cerr <<"ERROR: lcm is not good()" <<endl;
   }
   
-  RealtimeMF_lcm app(lcm,pRtmf);
+  RealtimeMF_lcm app(lcm,pRtmf,outPath);
   while(0 == lcm->handle());
   return 0;
 }
